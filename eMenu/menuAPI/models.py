377fffab9c0bf968c259406bb51a37ed0c661130
deKupini <k.kupinski92@gmail.com>
2020-08-31 04:24:44 +0200

    Models of dishes and menu cards with migration scripts.

--- eMenu/menuAPI/models.py
+++ eMenu/menuAPI/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=120)
+    price = models.IntegerField()
+    preparation_time = models.IntegerField()
+    creation_date = models.DateField()
+    last_modified = models.DateField()
+    vegetarian = models.BooleanField()
+
+
+class MenuCard(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    desc = models.CharField(max_length=120)
+    creation_date = models.DateField()
+    last_modified = models.DateField()
+    dishes = models.ManyToManyField(Dish)
